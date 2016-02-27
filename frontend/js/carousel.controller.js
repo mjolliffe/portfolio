7fdd6a151fe630b3509158246deb8897af205241
Michelle Jolliffe <michelle_jolliffe@alumni.brown.edu>
2016-02-26 16:43:52 -0800

    Fixed carousel to include photos from projects service. Need to populate services with photos from each project. Also need to fix page next logic.

--- frontend/js/carousel.controller.js
+++ frontend/js/carousel.controller.js
@@ -4,24 +4,8 @@ angular.module('Portfolio')
 CarouselController.$inject = ['ProjectData', '$state', '$stateParams', '$scope'];
 
 function CarouselController (ProjectData, $state, $stateParams, $scope){
-  $scope.photos = $scope.projects[$stateParams.id].photos
-
-  $scope.myInterval = 5000;
+  $scope.slides = $scope.projects[$stateParams.id].slides
+  $scope.myInterval = 4000;
   $scope.noWrapSlides = false;
-  var slides = $scope.slides = [];
-  var currIndex = 0;
-
-  $scope.addSlide = function() {
-    // var newWidth = 600 + slides.length + 1;
-    slides.push({
-      image: 'assets/images/asia.png',
-      text: ['Nice image','Awesome photograph','That is so cool','I love that'][slides.length % 4],
-      id: currIndex++
-    });
-  };
-
-  for (var i = 0; i < 4; i++) {
-    $scope.addSlide();
-  }
 
 }
